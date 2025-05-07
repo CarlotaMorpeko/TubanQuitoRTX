d27eedd5cb6646de42c46942e8fa7b4d005de99e
rosagabriela <rosagabrielapina@gmail.com>
2025-05-07 10:56:59 -0700

    entities "User" class has been changed to "AccountHolder"

---
+++ src/main/java/mx/itson/tubanquito/entities/AccountHolder.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.tubanquito.entities;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ *
+ * @author rosagabriela
+ */
+public class AccountHolder {
+    public String name;
+    public String adress;
+    public String city;
+    @SerializedName("taxpayer-id")
+    public String taxPayed;
+    @SerializedName("zip-code")
+    public int zipCode;
+    
+    
+}
