34a3a7395ebbd3adf75c2899c6a2fdd7ff7051da
rosagabriela <rosagabrielapina@gmail.com>
2025-05-08 21:09:02 -0700

    Constants "CREDIT" and "DEBIT" were added to the TypeTransaction enum.

---
+++ src/main/java/mx/itson/tubanquito/enums/TypeTransaction.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.tubanquito.enums;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ *
+ * @author rosagabriela
+ */
+public enum TypeTransaction {
+    @SerializedName("1")
+            CREDIT,
+    @SerializedName("2")
+            DEBIT
+            
+            
+}
